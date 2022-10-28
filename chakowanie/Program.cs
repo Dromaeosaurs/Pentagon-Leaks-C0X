982c5cb04abe84c30bb281fc96c68b4d9517e120
Dromaeosaurs <82401427+Dromaeosaurs@users.noreply.github.com>
2022-10-28 21:56:09 +0200

    cum

---
+++ chakowanie/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Siema, to ja Infromejtik");
+for(int i = 0;i<101;i++)
+{
+    Console.WriteLine("HAKOWANIE PENTAGONU " + i + "% " + " sex");
+}
+Console.WriteLine("szymix nie umie C# ja jestem lepszy bo ja informejtik");
+
